1695210781e9e352786519399b4dd29f57604f35
KimChungO <kco796@naver.com>
2023-10-18 17:03:03 +0900

    [Add]API 유저 랭킹 조회 API 및 특정 게임의 특정 유저 랭킹 조회 API 추가

--- src/services/resultApi.js
+++ src/services/resultApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-// 댓글 조회
+// 특정 게임 유저 랭킹 조회
 export const getGameResult = async (gameId, params) => {
     const data = await axios
         .get(`${API_URL}/api/results/game/${gameId}`, { params })
@@ -15,3 +15,44 @@ export const getGameResult = async (gameId, params) => {
     console.log(data);
     return data;
 };
+
+// UserId로 랭킹 조회
+export const getUsersResult = async (userId) => {
+    const data = await axios
+        .get(`${API_URL}/api/results/user/${userId}`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            return error;
+        });
+    console.log(data);
+    return data;
+};
+
+// 특정 게임 특정 유저의 최고 점수 조회
+export const getUserResult = async (userId, gameId) => {
+    const data = await axios
+        .get(`${API_URL}/api/result/game/${gameId}/user/${userId}`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            return error;
+        });
+    console.log(data);
+    return data;
+};
+
+// Game 랭킹 조회
+// export const getGames = async (params) => {
+//     const data = await axios
+//         .get(`${API_URL}/api/games/`, { params })
+//         .then((response) => {
+//             return response.data;
+//         })
+//         .catch((error) => {
+//             return error;
+//         });
+//     return data;
+// };
