3d568000881e59aaa776feb4776c4495fcc94514
Kim Chung O <kco796@naver.com>
2023-10-18 12:48:09 +0900

    [Add]API 회원가입 및 이메일 인증 API 추가

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -1,58 +1,52 @@
+import axios from "axios";
+
 const API_URL = process.env.REACT_APP_API_URL;
 
-// 회원가입
-export const postUser = async (params) => {
-    const response = await fetch(`${API_URL}/api/user`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-    });
-    const data = await response.json();
-    return data;
-};
-
-// 로그인
-export const postLogin = async (params) => {
-    const response = await fetch(`${API_URL}/api/user/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-    });
-    const data = await response.json();
+// 이메일 인증 코드 전송
+export const postEmail = async (params) => {
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const data = await axios
+        .post(
+            `${API_URL}/api/email`,
+            { params },
+            {
+                headers: headers,
+            }
+        )
+        .then((response) => {
+            return response;
+        })
+        .catch((error) => {
+            return error;
+        });
     console.log(data);
     return data;
 };
 
-// 회원 정보 조회
-export const getUser = async (params) => {
-    const response = await fetch(`${API_URL}/api/user/${params}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data = await response.json();
+// 회원가입
+export const postUser = async (params) => {
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const data = await axios
+        .post(
+            `${API_URL}/api/user`,
+            { params },
+            {
+                headers: headers,
+            }
+        )
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            return error;
+        });
+    console.log(data);
     return data;
 };
-
-// 회원 정보 수정
-export const putUser = async (params, userId) => {
-    const response = await fetch(`${API_URL}/api/user/${userId}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-    });
-    const data = await response.json();
-    return data;
-};
-
-// 회원탈퇴
 
 // test
 export const apiTest = async () => {
