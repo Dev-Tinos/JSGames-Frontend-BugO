d957ae7bbdd4eb575d6b5132cc61106f875883c3
KimChungO <kco796@naver.com>
2023-10-20 19:13:05 +0900

    [Update]LoginPage 로그인 구현

--- src/components/molecules/LoginForm.js
+++ src/components/molecules/LoginForm.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Input from "../atoms/Input.js";
 import LoginButton from "../atoms/LoginButton.js";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { postLogin } from "../../services/userApi.js";
 
 const LoginStyle = styled.div`
     background-color: #d9d9d9;
@@ -24,26 +25,32 @@ const LoginStyle = styled.div`
 const LoginForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const Login = () => {
-        alert("email: " + email + "\n password: " + password);
+    const Login = async () => {
+        await postLogin({ email: email, password: password });
+        navigate("/");
     };
 
     return (
         <LoginStyle>
             <h2>로그인</h2>
-            <form>
-                <Input type="text" placeholder={"Email"} onChange={setEmail} />
-                <Input
-                    type="password"
-                    placeholder={"Password"}
-                    onChange={setPassword}
-                />
-                <LoginButton text="로그인" onClick={Login} />
-                <p className="signup">
-                    <Link to={"/signup"}>처음이신가요?</Link>
-                </p>
-            </form>
+            <Input
+                type="text"
+                placeholder={"Email"}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+                type="password"
+                placeholder={"Password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <LoginButton text="로그인" onClick={Login} />
+            <p className="signup">
+                <Link to={"/signup"}>처음이신가요?</Link>
+            </p>
         </LoginStyle>
     );
 };
