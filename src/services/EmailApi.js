629da8845560922d185dcbbc4351f5a2cfbffe97
KimChungO <kco796@naver.com>
2023-11-23 00:02:01 +0900

    [Add]API 이메일 api 추가

---
+++ src/services/EmailApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+// 이메일 인증 코드 전송
+export const postEmail = async (params) => {
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const data = await axios
+        .post(`${API_URL}/api/email`, params, {
+            headers: headers,
+        })
+        .then((response) => {
+            console.log("인증 코드가 발송되었습니다.");
+            return response;
+        })
+        .catch((error) => {
+            alert(error.response.data.message);
+            return error;
+        });
+    return data;
+};
