4726160354849c7f0c9e178d991d93db567317f0
KimChungO <kco796@naver.com>
2023-11-28 22:33:46 +0900

    [Update]MainPage 메인페이지 무한스크롤 기능 추가

--- src/components/Templat/MainTemplat.js
+++ src/components/Templat/MainTemplat.js
@@ -2,11 +2,12 @@ import React from "react";
 import TopNavigationBar from "../organisms/TopNavigationBar";
 import GameList from "../organisms/GameList";
 
-const MainTemplat = ({ data, isLoading }) => {
+const MainTemplat = ({ data, isLoading, loaderRef }) => {
     return (
         <div>
             <TopNavigationBar />
             <GameList data={data} isLoading={isLoading} />
+            <div ref={loaderRef}>Loading</div>
         </div>
     );
 };
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MainTemplat from "../components/Templat/MainTemplat";
 import { getGameRanking } from "../services/RankingApi";
 
 const Main = () => {
     const [gameList, setGameList] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const loaderRef = useRef(null);
 
     useEffect(() => {
         const getData = async () => {
@@ -21,9 +23,38 @@ const Main = () => {
         getData();
     }, []);
 
+    useEffect(() => {
+        const fetchMoreData = async () => {
+            try {
+                const newList = await getGameRanking({ page: page, size: 12 });
+                setGameList((prevList) => [...prevList, ...newList]);
+                setPage((prevPage) => prevPage + 1);
+            } catch (error) {
+                console.error();
+            }
+        };
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const firstEntry = entries[0];
+                if (firstEntry.isIntersecting) {
+                    fetchMoreData();
+                }
+            },
+            { threshold: 1.0 }
+        );
+        if (loaderRef.current) {
+            observer.observe(loaderRef.current);
+        }
+        return () => observer.disconnect();
+    }, [loaderRef, page]);
+
     return (
         <div>
-            <MainTemplat data={gameList} isLoading={isLoading} />
+            <MainTemplat
+                data={gameList}
+                isLoading={isLoading}
+                loaderRef={loaderRef}
+            />
         </div>
     );
 };
