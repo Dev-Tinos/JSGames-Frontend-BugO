965153cd30329a23027320518978c00d8b7e3632
Kim Chung O <kco796@naver.com>
2023-12-19 15:20:05 +0900

    [Add]API 리뷰수정 api 추가

--- src/services/ReviewApi.js
+++ src/services/ReviewApi.js
@@ -4,8 +4,29 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 // 리뷰 조회
 export const getReviewList = async (gameId, params) => {
+    console.log(params);
     const data = await axios
         .get(`${API_URL}/review/game/${gameId}`, { params })
+        .then((response) => {
+            console.log(response);
+            return response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+            return error;
+        });
+    return data;
+};
+
+// 리뷰 작성
+export const postReview = async (params) => {
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const data = await axios
+        .post(`${API_URL}/review`, params, {
+            headers: headers,
+        })
         .then((response) => {
             return response.data;
         })
@@ -15,12 +36,13 @@ export const getReviewList = async (gameId, params) => {
     return data;
 };
 
-export const postReview = async (params) => {
+// 리뷰 수정
+export const putReview = async (reviewId, params) => {
     const headers = {
         "Content-Type": "application/json",
     };
     const data = await axios
-        .post(`${API_URL}/review`, params, {
+        .put(`${API_URL}/review/${reviewId}`, params, {
             headers: headers,
         })
         .then((response) => {
