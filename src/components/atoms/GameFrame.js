cb932b3c1c3ffd7e93213c1c3b25b2e8cb66d04d
KimChungO <kco796@naver.com>
2023-12-28 19:45:24 +0900

    [Update]GamePage gameUrl이 작동하는지 확인 후 iframe 출력

--- src/components/atoms/GameFrame.js
+++ src/components/atoms/GameFrame.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const FrameStyle = styled.div`
@@ -14,9 +14,35 @@ const FrameStyle = styled.div`
 `;
 
 const GameFrame = ({ gameUrl }) => {
+    const [error, setError] = useState(null);
+
+    const isProtocolValid = (url) => {
+        try {
+            const urlObject = new URL(url);
+            return (
+                urlObject.protocol === "http:" ||
+                urlObject.protocol === "https:"
+            );
+        } catch (error) {
+            // URL 파싱 중 에러가 발생하면 프로토콜이 아닌 것으로 처리
+            return false;
+        }
+    };
+
+    const handleIframeError = () => {
+        setError("Invalid protocol. Please use HTTP or HTTPS.");
+    };
     return (
         <FrameStyle>
-            <iframe src={gameUrl} title="게임 화면" />
+            {error ? (
+                <p>{error}</p>
+            ) : (
+                <iframe
+                    src={isProtocolValid(gameUrl) ? gameUrl : ""}
+                    title="게임 화면"
+                    onError={handleIframeError}
+                ></iframe>
+            )}
         </FrameStyle>
     );
 };
