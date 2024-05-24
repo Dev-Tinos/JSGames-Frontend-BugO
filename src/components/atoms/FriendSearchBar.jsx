8cd1a8b8a9240984e07a9e99cabe06b2c2131c5d
KimChungO <kco796@naver.com>
2024-05-24 14:39:19 +0900

    [Update]FriendPage inputbox css

--- src/components/atoms/FriendSearchBar.jsx
+++ src/components/atoms/FriendSearchBar.jsx
@@ -5,8 +5,16 @@ const InputBox = styled.div`
     width: 600px;
     height: 48px;
     margin: 30px;
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+    i {
+        position: absolute;
+        margin: 10px;
+        color: #999999;
+    }
     input {
-        width: 600px;
+        width: 100%;
         height: 48px;
         font-size: 16px;
         background-color: #f2f2f2;
@@ -14,13 +22,16 @@ const InputBox = styled.div`
         border-radius: 10px;
         border: 0px;
         color: #999999;
+        padding-left: 30px;
     }
 `;
 
 const FriendSearchBar = ({ value, handleChange, activeEnter }) => {
     return (
         <InputBox>
+            <i class="fas fa-search"></i>
             <input
+                className="search-bar__input"
                 type="text"
                 value={value}
                 onChange={handleChange}
