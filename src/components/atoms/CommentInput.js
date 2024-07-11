b83c4b785ecab38bf4267b8bf39200690456bfa9
KimChungO <kco796@naver.com>
2024-07-12 06:13:48 +0900

    [Update]댓글 입력창 높이 자동 조절 기능 추가

--- src/components/atoms/CommentInput.js
+++ src/components/atoms/CommentInput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const InputBox = styled.textarea`
@@ -7,7 +7,10 @@ const InputBox = styled.textarea`
     border-radius: 10px;
     width: 100%;
     resize: none;
-    padding: 20px;
+    padding: 30px;
+    overflow: hidden;
+    font-family: "SUIT-Regular", sans-serif;
+    font-size: 18px;
     &:focus {
         border: 2px solid #5383e8;
         box-shadow: 0 0 5px #5383e8;
@@ -16,11 +19,21 @@ const InputBox = styled.textarea`
 `;
 
 const CommentInput = ({ type, placeholder, onChange, value }) => {
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    }, [value]);
+
     const onhandleChange = (e) => {
         onChange(e.target.value);
     };
     return (
         <InputBox
+            ref={textareaRef}
             type={type}
             value={value}
             onChange={onhandleChange}
